1891ba243370c5e9e6865d7aaaac6c8de16a8172
meesam4687 <meesam4687@gmail.com>
2025-03-20 01:03:15 +0530

    ui: edit starting message to warn for slow response times

--- src/MessageContainer.jsx
+++ src/MessageContainer.jsx
@@ -3,7 +3,7 @@ function MessageContainer() {
   return (
     <div className="messageContainer">
       <div className="messageContainer-spacer"></div>
-      <ChatBubble message="Hello, my name is Epithet" alignment="Left"/>
+      <ChatBubble message="Hello, my name is Epithet (bear with the slow response times please, i am running the ai on free tier backends)" alignment="Left"/>
     </div>
   );
 }
