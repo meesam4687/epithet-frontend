1ccad661ee21e49a088adb30f73799500fc6be4c
meesam4687 <meesam4687@gmail.com>
2025-03-28 21:19:35 +0530

    chore: include vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from './Navbar'
 import Chatbox from './Chatbox'
 import MessageContainer from './MessageContainer'
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <div className='main'>
     <Navbar />
     <MessageContainer />
     <Chatbox />
+    <Analytics />
     </div>
   )
 }
