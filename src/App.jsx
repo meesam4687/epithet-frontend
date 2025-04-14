cc9a4466731393c2917db8c404b997a6c414306e
meesam4687 <meesam4687@gmail.com>
2025-04-14 11:35:32 +0530

    fix: remove session page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Session from './pages/Session';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Session />} />
       </Routes>
     </Router>
   );
